3b44d409d77132857cc13ab497cb29d47acf8b7a
Johnsiel Antonio Castanos Hernandez <johnsiel_castanos@ucne.edu.do>
2019-05-11 21:55:27 -0400

    agregando todo cap1

---
+++ EjerciciosLibroCSharp/cap1/eje1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace eje1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Johnsiel Castaños Hernandez");
+            Console.Read();
+        }
+    }
+}
---
+++ EjerciciosLibroCSharp/cap1/eje5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace eje5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola");
+            Console.WriteLine("Como estas?");
+            Console.WriteLine("Bien y tu?");
+            Console.WriteLine("Mas o Menos");
+            Console.Read();
+        }
+    }
+}
